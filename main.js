c64e32f6032fb99274cd3e2c4fa0e9431eb9609f
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-12-11 21:44:00 +0200

    Start using javascript validation api

--- main.js
+++ main.js
@@ -135,6 +135,7 @@ function createLibrary() {
     const submitBtn = document.querySelector('#submit');
     
     submitBtn.addEventListener('click', (e) => {
+        e.preventDefault();
         const form = document.querySelector('form');
         let formData = new FormData(form);
     
@@ -152,7 +153,19 @@ function createLibrary() {
             form.reset();
             modal.close();
             displayBooks();
-            e.preventDefault();
+        } else {
+            const title = document.getElementById('title');
+            const titleError = document.querySelector('label[for="title"] > span.error');
+            const author = document.getElementById('author');
+            const numPages = document.getElementById('num-pages');
+            const read = document.querySelector('radio');
+
+            if (title.validity.valueMissing) {
+                titleError.innerHTML = '<svg class="error-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>alert-circle</title><path d="M13,13H11V7H13M13,17H11V15H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg> Please enter a title';
+            } else {
+                titleError.innerHTML = null;
+            }
+
         }
     
     });
