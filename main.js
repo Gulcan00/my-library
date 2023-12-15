4056c4fd9508b19e048cd28b225564c4d4a66cfb
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-12-15 09:41:05 +0200

    Refactor form validation and error handling

--- main.js
+++ main.js
@@ -131,15 +131,38 @@ function createLibrary() {
     
     const cancelBtn = document.querySelector('#cancel');
     cancelBtn.addEventListener('click', () => modal.close());
+
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+
+    function setErrorMessages() {    
+        if (title.validity.valueMissing) {
+            title.setCustomValidity('Title is required');
+        } else {
+            title.setCustomValidity('');
+        }
+    
+        
+        if (author.validity.valueMissing) {
+            author.setCustomValidity('Author is required');
+        } else if (author.validity.patternMismatch) {
+            author.setCustomValidity('Please enter either the author\'s first name, last name, or both.');
+        } else {
+            author.setCustomValidity('');
+        }
+    }
+
+    title.addEventListener('input', setErrorMessages);
+    author.addEventListener('input', setErrorMessages);
     
     const submitBtn = document.querySelector('#submit');
     
     submitBtn.addEventListener('click', (e) => {
-        e.preventDefault();
         const form = document.querySelector('form');
         let formData = new FormData(form);
     
         if (form.checkValidity()) {
+            e.preventDefault();
             let values = [];
             for (const pair of formData.entries()) {
                 if (pair[0] === "read") {
@@ -154,18 +177,7 @@ function createLibrary() {
             modal.close();
             displayBooks();
         } else {
-            const title = document.getElementById('title');
-            const titleError = document.querySelector('label[for="title"] > span.error');
-            const author = document.getElementById('author');
-            const numPages = document.getElementById('num-pages');
-            const read = document.querySelector('radio');
-
-            if (title.validity.valueMissing) {
-                titleError.innerHTML = '<svg class="error-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>alert-circle</title><path d="M13,13H11V7H13M13,17H11V15H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg> Please enter a title';
-            } else {
-                titleError.innerHTML = null;
-            }
-
+            setErrorMessages();
         }
     
     });
